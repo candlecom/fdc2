cf4e50f94fc588fe47fa83d772729a5dfbceb7ed
Bruce <brucev@theedge.ca>
2017-01-24 17:06:31 -0700

    Files for header and top-box

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 
 gulp.task('default', function() {
@@ -11,7 +16,9 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-  console.log("Imagine Me!");
+  return gulp.src('./app/assets/styles/style.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
